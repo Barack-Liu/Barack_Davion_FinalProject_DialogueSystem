99592001dffeab0e2922829f39295d5000f74c9d
Barack <yanghongliu2013@outlook.com>
2023-03-14 16:51:24 -0700

    Update 2 Demo JSON

--- src/engine/dialogue/dialogue.js
+++ src/engine/dialogue/dialogue.js
@@ -8,6 +8,10 @@
 import DialogueUI from "./dialogue_ui.js";
 import DialogueText from "./dialogue_text.js";
 import engine from "../index.js";
+import { isButtonPressed } from "../input.js";
+
+//Define a const parent node according to the first 'title' element
+const buttonParentNode = document.querySelector('b').parentNode;
 
 class Dialogue {
     constructor(largeBg, avatar, textBg, nameText, dialogueText) {
@@ -23,6 +27,35 @@ class Dialogue {
         this.kNameBg = textBg;
         this.kDialogueText = dialogueText;
         this.kNameText = nameText;
+
+        //Player's attribute
+        this.kHealth = "";
+        this.kQi = "";
+        this.kAttack = "";
+        this.kDefend = "";  
+
+        this.kHealthValue = 0;
+        this.kQiValue = 0;
+        this.kAttackValue = 0;
+        this.kDefendValue = 0;   
+
+        //Add attribute 1
+        this.kHealthAdd1 = 0;
+        this.kQiAdd1 = 0;
+        this.kAttackAdd1 = 0;
+        this.kDefendAdd1 = 0;           
+
+        //Add attribute 2
+        this.kHealthAdd2 = 0;
+        this.kQiAdd2 = 0;
+        this.kAttackAdd2 = 0;
+        this.kDefendAdd2 = 0; 
+        
+        //Add attribute 3
+        this.kHealthAdd3 = 0;
+        this.kQiAdd3 = 0;
+        this.kAttackAdd3 = 0;
+        this.kDefendAdd3 = 0; 
 
         //Init large bg
         let hLargeBg = new engine.TextureRenderable(this.kLargeBg);
@@ -70,7 +103,66 @@ class Dialogue {
          this.mDialogueText3 = new engine.FontRenderable(this.kDialogueText);
          this.mDialogueText3.setColor([0.3, 0.6, 0.9, 1]);
          this.mDialogueText3.getXform().setPosition(21, 30);
-         this.mDialogueText3.setTextHeight(3);          
+         this.mDialogueText3.setTextHeight(3);   
+         
+         //Current Number
+         this.mCurrentNumber = 1;
+
+         //Have Option
+         this.mHaveOption = false;
+
+         //Option1
+         this.mOptionText1 = "";
+         this.mOptionNextNumber1 = 2;
+
+         //Option2
+         this.mOptionText2 = "";
+         this.mOptionNextNumber2 = 2;
+         
+         //Option3
+         this.mOptionText3 = "";
+         this.mOptionNextNumber3 = 2; 
+         
+         //Check wether option is drawn
+         this.mIsOptionDrawn = false;
+
+        //Health
+        this.mHealth = new engine.FontRenderable(this.kHealth);
+        this.mHealth.setColor([0.1, 0.1, 0.1, 1]);
+        this.mHealth.getXform().setPosition(10,95);
+        this.mHealth.setTextHeight(3);     
+        this.mHealth.setText("Health:" + this.kHealthValue);
+
+        //Qi
+        this.mQi = new engine.FontRenderable(this.kQi);
+        this.mQi.setColor([0.1, 0.1, 0.1, 1]);
+        this.mQi.getXform().setPosition(10,90);
+        this.mQi.setTextHeight(3);   
+        this.mQi.setText("Qi:" + this.kQiValue);        
+
+        //Attack
+        this.mAttack = new engine.FontRenderable(this.kAttack);
+        this.mAttack.setColor([0.1, 0.1, 0.1, 1]);
+        this.mAttack.getXform().setPosition(160,95);
+        this.mAttack.setTextHeight(3);     
+        this.mAttack.setText("Attack:" + this.kAttackValue);
+
+        //Defend
+        this.mDefend = new engine.FontRenderable(this.kDefend);
+        this.mDefend.setColor([0.1, 0.1, 0.1, 1]);
+        this.mDefend.getXform().setPosition(160,90);
+        this.mDefend.setTextHeight(3);   
+        this.mDefend.setText("Defend:" + this.kDefendValue); 
+
+        //Effect mode
+        this.mEffectMode1 = 0;
+        this.mEffectMode2 = 0;
+        this.mEffectMode3 = 0;      
+        
+        //Oscillate player
+        this.mOscillatePlayer = null;
+        this.kWidth = 40;
+        this.kHeight = 60;
     }
 
     getXform() { 
@@ -90,8 +182,23 @@ class Dialogue {
     }
 
     update() {
-        // simple default behavior
-        let pos = this.getXform().getPosition();
+        //Check special effect
+        if(this.mOscillatePlayer !== null){
+            console.log("Start oscillating");
+
+            if(this.mOscillatePlayer.done()){
+                this.stopOscillatePlayer();
+                this.mAvatar.getXform().setSize(this.kWidth, this.kHeight);
+
+                console.log("When done, Avatar size:" + this.mAvatar.getXform().getSize());
+            }
+            else{
+                let s = this.mOscillatePlayer.getNext();
+                this.mAvatar.getXform().setSize(this.kWidth + s[0], this.kHeight + s[1]);
+                
+                console.log("During oscillating, Avatar size:" + this.mAvatar.getXform().getSize());
+            }
+        }
     }
 
     draw(cam) {
@@ -104,7 +211,77 @@ class Dialogue {
             this.mDialogueText1.draw(cam);
             this.mDialogueText2.draw(cam);
             this.mDialogueText3.draw(cam);
+
+            //Draw Button
+            if(this.mHaveOption && (!this.mIsOptionDrawn)){
+                this.drawOption(this.mOptionText1, this.mOptionText2, this.mOptionText3);
+                this.mIsOptionDrawn = true;
+            }
         }
+    }
+
+    drawOption(optTex1, optTex2, optTex3){
+        //Button 1
+        //console.log("button1 = " + n);
+        
+       // if(typeof button1 === 'undefined'){    
+            let button1 = document.createElement('button');
+            button1.textContent = optTex1;
+            button1.setAttribute('id', 'button1');
+            button1.setAttribute('class', 'my-button');
+            button1.style.fontSize = '30px';
+            button1.style.width = '593px';
+            button1.style.height = '100px';
+            button1.style.position = 'absolute';
+            button1.style.left = '593px';
+            button1.style.top = '200px';
+            document.body.appendChild(button1);   
+
+            console.log("Draw option1");
+
+            //Make ParentNode global;
+            window.buttonParentNode1 = document.body;
+            window.button1 = button1;
+       // }
+ 
+        //Button 2
+       // if(typeof button2 === 'undefined'){
+            let button2 = document.createElement('button');
+            button2.textContent = optTex2;
+            button2.setAttribute('id', 'button2');
+            button2.setAttribute('class', 'my-button');
+            button2.style.fontSize = '30px';
+            button2.style.width = '593px';
+            button2.style.height = '100px';
+            button2.style.position = 'absolute';
+            button2.style.left = '593px';
+            button2.style.top = '350px';
+            document.body.appendChild(button2);
+            //buttonParentNode.appendChild(button2);
+
+            //Make ParentNode global;
+            window.button2 = button2;            
+       // }
+
+
+        //Button 3
+      //  if(typeof button3 === 'undefined'){
+            let button3 = document.createElement('button');
+            button3.textContent = optTex3;
+            button3.setAttribute('id', 'button3');
+            button3.setAttribute('class', 'my-button');
+            button3.style.fontSize = '30px';
+            button3.style.width = '593px';
+            button3.style.height = '100px';
+            button3.style.position = 'absolute';
+            button3.style.left = '593px';
+            button3.style.top = '500px';
+            document.body.appendChild(button3);    
+            //buttonParentNode.appendChild(button3);
+
+            //Make ParentNode global;
+            window.button3 = button3;                 
+      //  }       
     }
 
     setLargeBg(larBg){
@@ -159,6 +336,83 @@ class Dialogue {
         this.mDialogueText3.setTextHeight(3);
     }
 
+    setCurrentNumber(curNum){
+        this.mCurrentNumber = curNum;
+    }
+
+    setHaveOption(havOpt){
+        this.mHaveOption = havOpt;
+    }
+
+    setPlayerAttribute(hea, qi, att, def){
+        this.kHealth = hea;
+        this.kQi = qi;
+        this.kAttack = att;
+        this.kDefend = def;
+    }
+
+    setOption1(optTex, optNexNum, HeaTex, addHea, QiTex, addQi, AttTex, addAtt, DefTex, addDef, effMod){
+        this.mOptionText1 = optTex;
+        this.mOptionNextNumber1 = optNexNum;
+        
+        //this.kHealth = HeaTex;
+        this.kHealthAdd1 = addHea;
+        //this.kQi = QiTex;
+        this.kQiAdd1 = addQi;
+        //this.kAttack = AttTex;
+        this.kAttackAdd1 = addAtt;
+        //this.kDefend = DefTex;
+        this.kDefendAdd1 = addDef;
+        
+        this.mEffectMode1 = effMod;
+    }
+
+    setOption2(optTex, optNexNum, HeaTex, addHea, QiTex, addQi, AttTex, addAtt, DefTex, addDef, effMod){
+        this.mOptionText2 = optTex;
+        this.mOptionNextNumber2 = optNexNum;
+
+        //this.kHealth = HeaTex;
+        this.kHealthAdd2 = addHea;
+        //this.kQi = QiTex;
+        this.kQiAdd2 = addQi;
+        //this.kAttack = AttTex;
+        this.kAttackAdd2 = addAtt;
+        //this.kDefend = DefTex;
+        this.kDefendAdd2 = addDef;
+
+        this.mEffectMode2 = effMod;
+    }
+
+    setOption3(optTex, optNexNum, HeaTex, addHea, QiTex, addQi, AttTex, addAtt, DefTex, addDef, effMod){
+        this.mOptionText3 = optTex;
+        this.mOptionNextNumber3 = optNexNum;
+
+        //this.kHealth = HeaTex;
+        this.kHealthAdd3 = addHea;
+        //this.kQi = QiTex;
+        this.kQiAdd3 = addQi;
+        //this.kAttack = AttTex;
+        this.kAttackAdd3 = addAtt;
+        //this.kDefend = DefTex;
+        this.kDefendAdd3 = addDef;   
+
+        this.mEffectMode3 = effMod;
+    }
+
+    //Oscillate player
+    oscillatePlayer(){
+        if(this.mOscillatePlayer == null){
+            this.mOscillatePlayer = new engine.Oscillate.OscillatePosition(20, 30, 4, 180);            
+        }
+    }
+
+    stopOscillatePlayer(){
+        this.mOscillatePlayer = null;
+    }
+    
+    //Oscillate the position of enermy
+
+    //Oscillate the size of enermy
 }
 
 export default Dialogue;
--- src/my_game/my_game_draw.js
+++ src/my_game/my_game_draw.js
@@ -12,22 +12,32 @@ MyGame.prototype.drawWorld = function(cam) {
         cam.setViewAndCameraMatrix();
 
         this.mBg.draw(cam);
-        
+
         //Draw all available dialogue
         for(let i = 0; i < this.mDialogueSet.length; i++){
             if(this.mDialogueSet[i].mVisible){
-                this.mDialogueSet[i].draw(cam); 
+                this.mDialogueSet[i].draw(cam);
+                
+                //Player's attribute
+                this.mHealth.draw(cam);
+                this.mQi.draw(cam);
+                this.mAttack.draw(cam);
+                this.mDefend.draw(cam);                
             }
         }
-    }
+}
     //
     // This is the draw function, make sure to setup proper drawing environment, and more
     // importantly, make sure to _NOT_ change any state.
+
 MyGame.prototype.draw = function() {
         // Step A: clear the canvas
         engine.clearCanvas([1, 1, 1, 1]); // clear to light gray
         this.drawWorld(this.mCamera);
         //this.mZoomCams.draw();
+        
+        //Draw for Class Dialog
+        //this.mDialog.draw(this.mCamera);
     }
 
 export default MyGame;
